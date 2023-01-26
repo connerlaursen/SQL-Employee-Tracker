a1c9663762abf6c98016c49eef7cc48ba4f06384
Conner Laursen <conner@Conners-MacBook-Air.local>
2023-01-25 21:03:24 -0800

    added userquestions for inquirer

--- server.js
+++ server.js
@@ -4,3 +4,62 @@ const mysql2 = require('mysql2')
 const consoleTable = require('console.table'); 
 
 require('dotenv').config()
+
+//user questions
+
+const userQuestions = () => {
+    inquirer.prompt ([
+      {
+        type: 'list',
+        name: 'actions', 
+        message: 'Please select an action',
+        choices: [
+            'View all departments', 
+            'View all roles', 
+            'View all employees', 
+            'Add a department', 
+            'Add a role', 
+            'Add an employee', 
+            'Update an employee role',
+            'No Action']
+      }
+    ])
+
+    .then((answers) => {
+        const { actions } = answers; 
+  
+        if (actions === "View all departments") {
+          showDepartments();
+        }
+  
+        if (actions === "View all roles") {
+          showRoles();
+        }
+  
+        if (actions === "View all employees") {
+          showEmployees();
+        }
+  
+        if (actions === "Add a department") {
+          addDepartment();
+        }
+  
+        if (actions === "Add a role") {
+          addRole();
+        }
+  
+        if (actions === "Add an employee") {
+          addEmployee();
+        }
+  
+        if (actions === "Update an employee role") {
+          updateEmployee();
+        }
+
+        if (actions === "No Action Needed") {
+          connection.end()
+      };
+    });
+  };
+
+  
