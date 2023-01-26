49fa992ac3cc7bc20d818937c2b1b30606c70a41
Conner Laursen <conner@Conners-MacBook-Air.local>
2023-01-25 20:40:37 -0800

    fixed error

--- db/seeds.sql
+++ db/seeds.sql
@@ -8,20 +8,12 @@ VALUES
 
 INSERT INTO role(title, salary, department_id)
 VALUES
-    ("sales_rep", 50000, 1),
-    ("sales_manager", 70000, 2)
+    ("sales_rep", 80000, 1),
     ("software_engineer", 100000, 2),
-    ("software_engineer_manager", 120000, 2),
-    ("accountant", 70000, 3)
-    ("finance_manager", 100000, 3);
+    ("finance_manager", 90000, 3);
 
 INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES
-    ("Conner", "Laursen", 4, NULL),
-    ("John", "Doe", 1, 3),
-    ("Elyse", "Kelsey", 2, NULL),
-    ("Austin", "Sears", 5, 5),
-    ("Ryan", "Pak", 6, NULL),
-    ("Gunnar", "Olson", 3, 1);
-
-
+    ("Conner", "Laursen", 3, NULL),
+    ("John", "Doe", 1, 1),
+    ("Elyse", "Kelsey", 2, 2)
